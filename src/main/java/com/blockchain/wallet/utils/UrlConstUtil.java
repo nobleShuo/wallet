4736aba7a2e8c583f7de10af45cae4352640c9de
qishuo <qis@yomob.com>
2019-02-22 18:27:27 +0800

    添加类

--- src/main/java/com/blockchain/wallet/utils/UrlConstUtil.java
+++ src/main/java/com/blockchain/wallet/utils/UrlConstUtil.java
@@ -15,15 +15,7 @@ public class UrlConstUtil {
     /**
      * 私有链eth节点的服务器
 //     */
-//    //@Value("${privateEth.server}")
-//    public static  List<String> ETH_NODE_LIST;
 
-//    static {
-//        ETH_NODE_LIST = new ArrayList<>();
-//        ETH_NODE_LIST.add("http://47.75.60.231:");
-//        ETH_NODE_LIST.add("http://47.75.50.204:");
-//        ETH_NODE_LIST.add("http://47.75.188.184:");
-//    }
     /**
      * 获取私钥的端口+地址
      */
@@ -31,11 +23,11 @@ public class UrlConstUtil {
     /**
      * 私有链节点地址
      */
-    // public static final String ETH_PRIVATE_NODE_URL = "http://47.75.60.231:8545";
+    // public static final String ETH_PRIVATE_NODE_URL = "";
     /**
      * node服务获取地址私钥
      */
-    // public static final String NODE_SERVICE_GET_PRIVATE_KEY = "http://47.75.60.231:8001/getPrivateKey";
+    // public static final String NODE_SERVICE_GET_PRIVATE_KEY = "";
     /**
      * node服务获取token余额
      */
@@ -43,6 +35,6 @@ public class UrlConstUtil {
     /**
      * 公有链节点地址(使用的nodeJS服务中的地址)
      */
-    public static final String ETH_PUBLIC_NODE_URL = "https://ropsten.infura.io/VlzsGEDfjeD3lw1JthgE";
+    public static final String ETH_PUBLIC_NODE_URL = "";
 
 }
